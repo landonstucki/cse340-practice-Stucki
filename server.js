23547b1b1f4199aca903ab1765270f28ffce923a
Landon <Landon@users.noreply.github.com>
2026-01-14 20:54:08 -0700

    Set up EJS layouts and fixed home page styling

---
+++ server.js
@@ -0,0 +1,49 @@
+// Imports
+import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+/**
+ * Declare Important Variables
+ */
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+const name = process.env.NAME; // <-- NEW
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/**
+ * Setup Express Server
+ */
+const app = express();
+
+/**
+ * Declare Routes
+ */
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    const title = 'Welcome Home';
+    res.render('home', { title });
+});
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
+});
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
